f68d92950a3240d4ba5549c624baa8b1d0be0cf1
David Tewkesbury <tewkesburyd@hotmail.com>
2022-10-13 18:47:42 -0600

    commit tree ae1cdfb0814514e047e4dd07b7c0445bcfb0f92d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(arrayOfStrings, event){
+    const messege = [ ];
+    for (let n = 0; n <= arrayOfStrings.length - 1; n += 1){
+        messege.push(`Thank you, ${arrayOfStrings[n]}, for the wonderful ${event} gift!`);
+    };
+    return messege;
+}
+
+function countDown(num){
+    while (num >= 0){
+        console.log(num);
+        num -=1;
+    };
+}
